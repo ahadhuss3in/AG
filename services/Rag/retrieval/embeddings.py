36d0f9523f35dfbf8435f00d2025862ade3aba22
ahadhuss3in <ahadasif.cs22@sahyadri.edu.in>
2026-08-30 23:10:13 +0300

    skeleton of the embedding logic

--- services/Rag/retrieval/embeddings.py
+++ services/Rag/retrieval/embeddings.py
@@ -4,6 +4,38 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from app.config import config
 
 BATCH_SIZE = 50 
-_GEMINI_DIM = 3072
-_FALLBACK_DIM = 758
+_GEMINI_DIM = 3072  #Dimention of embedding model ( it just means it has these many elements)
+_FALLBACK_DIM = 768 
 
+_active_model = None
+_model_type : str | None = None #gemini or fallback
+
+def _probe_gemini():
+    """a embedded call to verify gemini is running 
+    please use my other project which is a AI gateway to avoid this :)
+    """
+
+def load_fallback():
+    return
+
+def _init():
+    return
+
+def get_embedding_dim() -> int:
+    """returns the vec dim fir active model."""
+    return
+
+def embedding_batch(batch:list[str]) -> list[list[float]]:
+    """embedding a batch using the embedding model"""
+    return 
+
+
+## aha now the query should also be in same format as asnwer right?
+##so will also embedd the query so u can compare apple to apple in hdhahduh(perosnal work for embeddings) language
+
+def embedding_query(query:str) -> list[float]:
+    """embedded query to compare"""
+    return 
+
+def embedded_texts(texts:list[str])-> list[list[float]]: 
+    _init()
