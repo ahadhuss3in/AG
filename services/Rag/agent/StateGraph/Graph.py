2eb39276b34c66372e2516fa4fbb07681239eeff
ahadhuss3in <ahadasif.cs22@sahyadri.edu.in>
2026-09-04 19:57:00 +0300

    Graph nodes

--- services/Rag/agent/StateGraph/Graph.py
+++ services/Rag/agent/StateGraph/Graph.py
@@ -2,3 +2,18 @@
 
 
 ## connecting akk the components
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from services.Rag.agent.StateGraph.RagState import AgentState
+from services.Rag.agent.nodes.planner import planned_node
+from services.Rag.agent.nodes.responder import generate_node
+from services.Rag.agent.nodes.retriever import retrieve_node
+
+## Initialize the graph
+graph = StateGraph(AgentState)
+
+
+## define what are the nodes of the graph
+graph.add_node("planner",planned_node)
+graph.add_node("retriver", retrieve_node)
+graph.add_node("generate_res", generate_node)
